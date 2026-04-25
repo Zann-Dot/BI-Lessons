e08a271f8d191ffd5400bde6978721675e416451
Zann-Dot <anaykarn234@gmail.com>
2026-04-25 16:21:07 +0530

    create BI1.1 Hw2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Movies from "./lessons/BI1.1 CW/Movies";
 import Books from "./lessons/BI1.1 HW1/Books";
+import Hotels from "./lessons/BI1.1 HW2/Hotels";
 import { Route, Routes } from "react-router";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Movies />} />
         <Route path="/books" element={<Books />} />
+        <Route path="/hotels" element={<Hotels />} />
       </Routes>
     </>
   );
